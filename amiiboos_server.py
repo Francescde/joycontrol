1611ed3ac964a940ab94092920a425e9573150d0
Francesc de Paula de Puig Guixé <francescdepuig@gmail.com>
2023-02-18 21:39:48 +0100

    fix send comand

--- amiiboos_server.py
+++ amiiboos_server.py
@@ -94,7 +94,7 @@ def send_report():
 @app.route('/files', methods=['POST'])
 def get_files():
     content = request.json
-    print('content' + content.keys)
+    print('content', content.keys)
     folderpath = content.data.path
     return jsonify(
         [join(folderpath, f) for f in os.listdir(folderpath) if isfile(join(folderpath, f)) and ('.bin' in f)])
