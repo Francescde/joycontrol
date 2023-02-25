c4bfb284ba86e9477f586ee8dde4f842b1a049cf
Francesc de Paula de Puig Guixé <francescdepuig@gmail.com>
2023-02-25 17:32:34 +0100

    fix amiibo script

--- startup_amiibo_loader.py
+++ startup_amiibo_loader.py
@@ -9,6 +9,7 @@ async def load_script_for_amiibo_of_array(arrayPosition, amiiboArray, amiiboFold
     file = os.path.join(amiiboFolder, filename)
     os.system('sudo python3 load_amiibos.py -nfc=' + file + ' -script=' + script)
 
+
 async def load_amiibos(script, amiiboFolder):
     # wait for input to be accepted
     actual = 0
@@ -27,27 +28,31 @@ async def load_amiibos(script, amiiboFolder):
             await load_script_for_amiibo_of_array(actual, listD, amiiboFolder, script)
         actual += 1
 
+
 async def load_server(script, amiiboFolder):
     os.system('sudo python3 amiiboos_server.py -folder=' + amiiboFolder + ' -script=' + script)
+
 
 async def load_one(script, amiiboFolder):
     amiiboList = [filename for filename in os.listdir(amiiboFolder) if '.bin' in filename]
     message = "enter amiibo key\n"
     for k in range(len(amiiboList)):
-        message += "key: "+k+"amiibo: "+amiiboList[k]
+        message += "key: " + str(k) + "amiibo: " + amiiboList[k]
     indx = await ainput(prompt=message)
     await load_script_for_amiibo_of_array(indx, amiiboList, amiiboFolder, script)
+
 
 async def await_instruction(script, amiiboFolder):
     while True:
         message = "write server to launch server, all to execute the script for all amiibos, and one to load one amiibo\n"
         instruction = await ainput(prompt=message)
-        if(instruction=="server"):
+        if (instruction == "server"):
             await load_server(script, amiiboFolder)
-        if (instruction=="all"):
+        if (instruction == "all"):
             await load_amiibos(script, amiiboFolder)
-        if (instruction=="one"):
+        if (instruction == "one"):
             await load_one(script, amiiboFolder)
+
 
 if __name__ == '__main__':
     # check if root
