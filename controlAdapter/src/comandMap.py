5b6b734bff2b271ad224d28e68e5c2e9b8f5670a
Francesc de Paula de Puig Guixé <francescdepuig@gmail.com>
2023-05-20 13:07:42 +0200

    what?

--- controlAdapter/src/comandMap.py
+++ controlAdapter/src/comandMap.py
@@ -94,7 +94,7 @@ async def main(websocket):
 
 if __name__ == '__main__':
     async def connect():
-        async with websockets.connect('ws://localhost:8082') as websocket:
+        async with websockets.connect('ws://0.0.0.0:8082') as websocket:
             # Send a dictionary as JSON payload
             main(websocket)
 
