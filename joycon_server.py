2d253b25181966641dca3fb512dee7f3b9f22da1
Francesc de Paula de Puig Guixé <francescdepuig@gmail.com>
2023-05-21 22:55:39 +0200

    all async

--- joycon_server.py
+++ joycon_server.py
@@ -134,7 +134,7 @@ async def connected():
 
 
 @app.route('/execute_script', methods=['POST'])
-def executeScript():
+async def executeScript():
     global objectMap;
     content = request.get_json()
     if(objectMap['scriptRunning'] and not objectMap['scriptRunning'].done()):
@@ -147,7 +147,7 @@ def executeScript():
 
 
 @app.route('/script_running', methods=['GET'])
-def scriptRuning():
+async def scriptRuning():
     global objectMap;
     scriptRunnig = False;
     if(objectMap['scriptRunning']):
@@ -156,7 +156,7 @@ def scriptRuning():
 
 
 @app.route('/kill_script', methods=['GET'])
-def killScript():
+async def killScript():
     global objectMap;
     if(objectMap['scriptRunning'] and not objectMap['scriptRunning'].done()):
         objectMap['scriptRunning'].cancel()
@@ -260,12 +260,12 @@ async def disconnect():
 
 
 @app.route('/view/<controllerName>')
-def display_view(controllerName):
+async def display_view(controllerName):
     return render_template(controllerName+'.html', amiiboFolder=amiiboFolder, script=script )
 
 
 @app.route('/position_objects/<controllerName>')
-def set_controller_objects(controllerName):
+async def set_controller_objects(controllerName):
     # Opening JSON file
     f = open('controllers/'+controllerName+'.json')
     data = json.load(f)
@@ -276,7 +276,7 @@ def set_controller_objects(controllerName):
 
 
 @app.route('/controller/<controllerName>')
-def display_controller(controllerName):
+async def display_controller(controllerName):
     global readInterval
     # Opening JSON file
     f = open('controllers/'+controllerName+'.json')
@@ -287,7 +287,7 @@ def display_controller(controllerName):
 
 
 @app.route('/files', methods=['POST'])
-def get_files():
+async def get_files():
     content = request.get_json()
     print('content')
     print(content)
@@ -298,14 +298,14 @@ def get_files():
     return jsonify([])
 
 @app.route('/controllers', methods=['GET'])
-def get_controllers():
+async def get_controllers():
     folderpath = 'controllers'
     return jsonify(
         [f for f in os.listdir(folderpath) if isfile(join(folderpath, f)) and ('.json' in f)])
 
 
 @app.route('/controllers/<controllerName>')
-def get_controller(controllerName):
+async def get_controller(controllerName):
     # Opening JSON file
     f = open('controllers/'+controllerName+'.json')
     data = json.load(f)
@@ -316,7 +316,7 @@ def get_controller(controllerName):
 
 
 @app.route('/delete_controller/<controllerName>')
-def delete_controller(controllerName):
+async def delete_controller(controllerName):
     path = os.path.join('controllers', controllerName+'.json')  
     os.remove(path)
     return jsonify({
@@ -325,7 +325,7 @@ def delete_controller(controllerName):
 
 
 @app.route('/controllers', methods=['POST'])
-def add_controllers():
+async def add_controllers():
     content = request.get_json()
     print('content')
     print(content)
@@ -339,14 +339,14 @@ def add_controllers():
 
 
 @app.route('/scripts', methods=['GET'])
-def get_scripts():
+async def get_scripts():
     folderpath = 'rjctScripts'
     return jsonify(
         [f for f in os.listdir(folderpath) if isfile(join(folderpath, f)) and ('.txt' in f)])
 
 
 @app.route('/scripts/<controllerName>')
-def get_script(controllerName):
+async def get_script(controllerName):
     # Opening JSON file
     f = open('rjctScripts/'+controllerName+'.txt')
     data = f.read()
@@ -357,7 +357,7 @@ def get_script(controllerName):
 
 
 @app.route('/scripts', methods=['POST'])
-def add_scripts():
+async def add_scripts():
     content = request.get_json()
     print('content')
     print(content)
@@ -371,7 +371,7 @@ def add_scripts():
 
 
 @app.route('/delete_script/<controllerName>')
-def delete_script(controllerName):
+async def delete_script(controllerName):
     path = os.path.join('rjctScripts', controllerName+'.txt')  
     os.remove(path)
     return jsonify({
@@ -380,7 +380,7 @@ def delete_script(controllerName):
 
 # SocketIO event handler for WebSocket connections
 @socketio.on('connect')
-def handle_connect():
+async def handle_connect():
     print('Client connected')
 
 @socketio.on('message')
@@ -392,7 +392,7 @@ async def handle_message(message):
     # Process the message or send a response back to the client
 
 @socketio.on('disconnect')
-def handle_disconnect():
+async def handle_disconnect():
     print('Client disconnected')
 
 if __name__ == '__main__':
