5b4a147be272c906965be3b25dac494a8ff6fe0d
Francesc de Paula de Puig Guixé <francescdepuig@gmail.com>
2023-05-22 02:09:35 +0200

    fix some shit

--- controlAdapter/src/comandMap.py
+++ controlAdapter/src/comandMap.py
@@ -74,16 +74,16 @@ def main():
         buttons_prev = buttons
         if((not are_close_values(l_stick_values['v'], l_stick[0], l_stick_values['precision'])) or (not are_close_values(l_stick_values['h'], -l_stick[1], l_stick_values['precision']))):
             print('l_stick ' + str(l_stick[0]) +", "+ str(-l_stick[1]))
-            l_stick_values['v']=l_stick[0]
-            l_stick_values['h']=-l_stick[1]
+            l_stick_values['h']=l_stick[0]
+            l_stick_values['v']=-l_stick[1]
             response = requests.post('http://localhost:8082/analog', json = { 'key': 'l','vertical': rule_of_three(l_stick_values['v']),'horizontal': rule_of_three(l_stick_values['h'])})
             print(response)
             #emit event on websocket
         if((not are_close_values(r_stick_values['v'], r_stick[0], r_stick_values['precision'])) or (not are_close_values(r_stick_values['h'], -r_stick[1], r_stick_values['precision']))):
             print('r_stick ' + str(r_stick[0]) +", "+ str(-r_stick[1]))
-            r_stick_values['v']=r_stick[0]
-            r_stick_values['h']=-r_stick[1]
-            response = requests.post('http://localhost:8082/analog', json = { 'key': 't', 'vertical': rule_of_three(r_stick_values['v']), 'horizontal': rule_of_three(r_stick_values['h'])})
+            r_stick_values['h']=r_stick[0]
+            r_stick_values['v']=-r_stick[1]
+            response = requests.post('http://localhost:8082/analog', json = { 'key': 'r', 'vertical': rule_of_three(r_stick_values['v']), 'horizontal': rule_of_three(r_stick_values['h'])})
             print(response)
             #emit event on websocket
     print('Initializing Nintendo Switch Pro Controller... ', end='', flush=True)
