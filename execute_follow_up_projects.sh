a65ca1844ed8adf426823a65a87df330f49acbfd
Francesc de Paula de Puig Guixé <francescdepuig@gmail.com>
2023-06-02 18:47:09 +0200

    await a little bit

--- execute_follow_up_projects.sh
+++ execute_follow_up_projects.sh
@@ -8,7 +8,10 @@ else
     echo "./clientCtr does not exist on your filesystem."
     sudo cp controlAdapter/udev/* /etc/udev/rules.d
     python3 -m venv clientCtr
+    sleep 10
     source clientCtr/bin/activate
+    sleep 10
     pip install hidapi==0.7.99.post21 requests
 fi
+sleep 10
 python3 controlAdapter/src/comandMap.py &
