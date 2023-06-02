15651d7f3fe39fb9ea7ea366f33e394964ecfafe
Francesc de Paula de Puig Guixé <francescdepuig@gmail.com>
2023-06-02 19:08:36 +0200

    si no a la primera a la segona tu

--- execute_follow_up_projects.sh
+++ execute_follow_up_projects.sh
@@ -3,15 +3,12 @@ sudo chmod -R 777 .
 if [[ -d ./clientCtr ]]
 then
     echo "./clientCtr exists on your filesystem."
-    source /home/pi/joycontrol/clientCtr/bin/activate 
+    source clientCtr/bin/activate 
+    pip install hidapi==0.7.99.post21 requests
+    python3 controlAdapter/src/comandMap.py &
 else
     echo "./clientCtr does not exist on your filesystem."
     sudo cp controlAdapter/udev/* /etc/udev/rules.d
     python3 -m venv clientCtr
     sleep 10
-    source /home/pi/joycontrol/clientCtr/bin/activate
-    sleep 10
-    pip install hidapi==0.7.99.post21 requests
 fi
-sleep 10
-python3 controlAdapter/src/comandMap.py &
