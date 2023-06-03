f9d42c1d9caa697dbae025d6a1c6956f675df3e1
Francesc de Paula de Puig Guixé <francescdepuig@gmail.com>
2023-06-03 14:13:42 +0200

    reduce precision furder

--- controlAdapter/src/comandMap.py
+++ controlAdapter/src/comandMap.py
@@ -49,14 +49,14 @@ def main():
         "centerRadius": 1000,
         "v": 0,
         "h": 0,
-        "precision": 10000
+        "precision": 15000
     }
     r_stick_values = {
         "center": True,
         "centerRadius": 1000,
         "v": 0,
         "h": 0,
-        "precision": 10000
+        "precision": 15000
     }
     def send_to_controller(buttons, l_stick, r_stick, _, __, ___):
         nonlocal buttons_prev, l_stick_values, r_stick_values
